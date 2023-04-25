0ad215d2e290da46824d4dc26bc50b864f2a4de1
Will Matheus <dev.owillmatheus@gmail.com>
2023-04-25 09:34:20 -0300

    versão 13.0.0

--- src/paginas/principal/PaginaPrincipal.tsx
+++ src/paginas/principal/PaginaPrincipal.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./PaginaPrincipal.css";
 
-
 function PaginaPrincipal() {
   useEffect(() => {
     document.body.classList.add("fundo-gradiente");
@@ -11,66 +10,58 @@ function PaginaPrincipal() {
     };
   }, []);
 
-  return (
-    <>
+    return (
+      <>
       <div className="container">
         <h1 className="titulo">Salve Motoca!</h1>
         <h2 className="subtitulo">O que você deseja fazer?</h2>
         <div className="botoes">
           <div className="row">
             <div className="col-md-6">
-              <a href="/registrar">
-                <div className="botao">
-                  <img
-                    src="https://tinypic.host/images/2023/04/22/icons8-editar-propriedade-64.png"
-                    alt="Icone 1"
-                    className="icone"
-                  />
-                  <p className="texto-botao">Registrar valores e km</p>
-                </div>
-              </a>
+              <Link to="/registrar" className="botao">
+                <img
+                  src="https://tinypic.host/images/2023/04/22/icons8-editar-propriedade-64.png"
+                  alt="Icone 1"
+                  className="icone"
+                />
+                <p className="texto-botao">Registrar valores e km</p>
+              </Link>
             </div>
             <div className="col-md-6">
-              <a href="/historico">
-                <div className="botao">
-                  <img
-                    src="https://tinypic.host/images/2023/04/22/icons8-historico-de-encomendas-100.png"
-                    alt="Icone 2"
-                    className="icone"
-                  />
-                  <p className="texto-botao">Ver histórico</p>
-                </div>
-              </a>
+              <Link to="/historico" className="botao">
+                <img
+                  src="https://tinypic.host/images/2023/04/22/icons8-historico-de-encomendas-100.png"
+                  alt="Icone 2"
+                  className="icone"
+                />
+                <p className="texto-botao">Ver histórico</p>
+              </Link>
             </div>
             <div className="col-md-6">
-              <a href="/metas">
-                <div className="botao">
-                  <img
-                    src="https://tinypic.host/images/2023/04/22/icons8-objetivo-100.png"
-                    alt="Icone 3"
-                    className="icone"
-                  />
-                  <p className="texto-botao">Definir metas</p>
-                </div>
-              </a>
+              <Link to="/metas" className="botao">
+                <img
+                  src="https://tinypic.host/images/2023/04/22/icons8-objetivo-100.png"
+                  alt="Icone 3"
+                  className="icone"
+                />
+                <p className="texto-botao">Definir metas</p>
+              </Link>
             </div>
             <div className="col-md-6">
-              <a href="/km">
-                <div className="botao">
-                  <img
-                    src="https://tinypic.host/images/2023/04/22/icons8-objetivo-100.png"
-                    alt="Icone 4"
-                    className="icone"
-                  />
-                  <p className="texto-botao">Troca de óleo</p>
-                </div>
-              </a>
+              <Link to="/km" className="botao">
+                <img
+                  src="https://tinypic.host/images/2023/04/22/icons8-objetivo-100.png"
+                  alt="Icone 4"
+                  className="icone"
+                />
+                <p className="texto-botao">Troca de óleo</p>
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </>
-  );
-}
-
-export default PaginaPrincipal;
+      </>
+    );
+  }
+  
+  export default PaginaPrincipal;
